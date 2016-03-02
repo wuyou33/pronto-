a36ff2345bacb5bd0cb9d1985ebc58dd8ca90e65
Marco Camurri <marco.camurri@iit.it>
2016-03-02 15:10:06 +0100

    Updated translators for hyq
    Conflicts:
    	externals/Eigen_pod
    	externals/common_utils
    	externals/kinematics-utils
    	externals/lcm-pod
    	externals/occ-map
    	externals/vicon
    	optional/ICPCUDA-pod
    	pronto-lcm-ros-translators/src/pronto_translators/src/ros2lcm_hyq.cpp

--- pronto-lcm-ros-translators/src/pronto_translators/src/ros2lcm_hyq.cpp
+++ pronto-lcm-ros-translators/src/pronto_translators/src/ros2lcm_hyq.cpp
@@ -19,9 +19,10 @@
 #include <geometry_msgs/Wrench.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TransformStamped.h>
-#include <std_msgs/Float64.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Int32.h>
+#include <nav_msgs/Odometry.h>
+
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
 
 #include <lcm/lcm-cpp.hpp>
 #include <lcmtypes/bot_core.hpp>
@@ -42,37 +43,86 @@ public:
   ~App();
 
 private:
-  bool send_ground_truth_; // publish control msgs to LCM
-  lcm::LCM lcm_publish_ ;
-  ros::NodeHandle node_;
-  
-  // Atlas Joints and FT sensor
-  ros::Subscriber  joint_states_sub_;  
-  void joint_states_cb(const sensor_msgs::JointStateConstPtr& msg);  
-  std::vector<double> ptu_position_,ptu_velocity_,ptu_effort_;
-  std::vector<std::string> ptu_name_;
-  
-  // The position and orientation from BDI's own estimator:
-  ros::Subscriber pose_bdi_sub_;
-  void pose_bdi_cb(const nav_msgs::OdometryConstPtr& msg);
+    Eigen::Affine3d bTf; // Transform between viconplate-to-body
+    bool first_vicon_transform;
+    bool send_ground_truth_; // publish control msgs to LCM
+    bool send_pose_body_;
+    lcm::LCM lcmPublish_ ;
+    ros::NodeHandle node_;
 
-  // The position and orientation from a vicon system:
-  ros::Subscriber pose_vicon_sub_;
-  void pose_vicon_cb(const geometry_msgs::TransformStampedConstPtr& msg);
+    // Atlas Joints and FT sensor
+    ros::Subscriber  joint_states_sub_;
+    void joint_states_cb(const sensor_msgs::JointStateConstPtr& msg);
+    std::vector<double> ptu_position_, ptu_velocity_, ptu_effort_;
+    std::vector<std::string> ptu_name_;
 
-  ros::Subscriber imuSensorSub_;
-  void imuSensorCallback(const sensor_msgs::ImuConstPtr& msg);
+    // The position and orientation from a vicon system:
+    ros::Subscriber pose_vicon_sub_;
+    ros::Subscriber pose_sim_ground_truth;
+    void pose_vicon_cb(const geometry_msgs::TransformStampedConstPtr& msg);
 
-  int64_t last_joint_state_utime_;
-  bool verbose_;
+    ros::Subscriber imuSensorSub_;
+    void imuSensorCallback(const sensor_msgs::ImuConstPtr& msg);
+
+    ros::Subscriber laserScanSub_;
+    void laserScanCallback(const sensor_msgs::LaserScanConstPtr& msg);
+
+    void simGroundTruthCallback(const nav_msgs::OdometryConstPtr& msg);
+
+    void publishLidar(const sensor_msgs::LaserScanConstPtr& msg, string channel );
+
+    int64_t last_joint_state_utime_;
+    bool verbose_;
+    mav::ins_t imu;
+    bot_core::pose_t pose_ground_truth;
+    bot_core::rigid_transform_t transf_ground_truth;
+
+    tf::TransformListener listener_;
+    ViconDerivator* vd_;
 };
 
-App::App(ros::NodeHandle node_, bool send_ground_truth_) :
-    send_ground_truth_(send_ground_truth_), node_(node_){
-  ROS_INFO("Initializing Translator");
-  if(!lcm_publish_.good()){
-    std::cerr <<"ERROR: lcm is not good()" <<std::endl;
-  }
+App::App(ros::NodeHandle& node,
+         bool send_ground_truth,
+         bool send_pose_body) :
+
+    send_ground_truth_(send_ground_truth),
+    node_(node),
+    send_pose_body_(send_pose_body),
+    first_vicon_transform(true),
+    bTf(Eigen::Affine3d::Identity()),
+    vd_(new ViconDerivator(5)) {
+
+    ROS_INFO("Initializing Translator");
+    if(!lcmPublish_.good()) {
+        std::cerr << "ERROR: lcm is not good()" << std::endl;
+    }
+
+    for(int i = 0; i < 4; i++) {
+        imu.quat[i] = 0;
+        if(i < 3) {
+            imu.accel[i] = 0;
+            imu.gyro[i] = 0;
+            imu.mag[i] = 0;
+        }
+    }
+    imu.pressure = 0;
+    imu.rel_alt = 0;
+
+    joint_states_sub_ = node_.subscribe(string("joint_states"), 100, &App::joint_states_cb, this);
+    ptu_name_ = {"ptu_pan", "ptu_tilt"};
+    ptu_position_ = {0.0, 0.0};
+    ptu_velocity_ = {0.0, 0.0};
+    ptu_effort_ = {0.0, 0.0};
+
+    pose_vicon_sub_ = node_.subscribe(vicon_frame, 100, &App::pose_vicon_cb, this);
+    pose_vicon_sub_ = node_.subscribe("/hyq2max/ground_truth_odom", 100, &App::simGroundTruthCallback, this);
+    imuSensorSub_ = node_.subscribe("/imu/imu", 100, &App::imuSensorCallback, this);
+
+    laserScanSub_ = node_.subscribe("scan", 100, &App::laserScanCallback, this);
+
+    verbose_ = false;
+    ROS_INFO("ros2lcm Translator ready");
+}
 
   joint_states_sub_ = node_.subscribe(string("joint_states"), 100, &App::joint_states_cb,this);
   ptu_name_ = {"ptu_pan", "ptu_tilt"};
@@ -80,13 +130,168 @@ App::App(ros::NodeHandle node_, bool send_ground_truth_) :
   ptu_velocity_ = {0.0,0.0};
   ptu_effort_ = {0.0,0.0};
 
-  pose_vicon_sub_ = node_.subscribe(string("vicon/hyq/body"), 100, &App::pose_vicon_cb,this);
-  imuSensorSub_ = node_.subscribe(string("Imu"), 100, &App::imuSensorCallback,this);
+void App::simGroundTruthCallback(const nav_msgs::OdometryConstPtr &msg) {
+    pose_ground_truth.utime = msg->header.stamp.toNSec() / 1000;
+
+    Eigen::Affine3d world_to_base = Eigen::Affine3d::Identity();
+    Eigen::Quaterniond msg_orient(msg->pose.pose.orientation.w,
+                                  msg->pose.pose.orientation.x,
+                                  msg->pose.pose.orientation.y,
+                                  msg->pose.pose.orientation.z);
+    world_to_base.rotate(msg_orient);
+
+    pose_ground_truth.pos[0] = msg->pose.pose.position.x;
+    pose_ground_truth.pos[1] = msg->pose.pose.position.y;
+    pose_ground_truth.pos[2] = msg->pose.pose.position.z;
+
+    pose_ground_truth.orientation[0] = msg->pose.pose.orientation.w;
+    pose_ground_truth.orientation[1] = msg->pose.pose.orientation.x;
+    pose_ground_truth.orientation[2] = msg->pose.pose.orientation.y;
+    pose_ground_truth.orientation[3] = msg->pose.pose.orientation.z;
+
+    // Velocity must be in the base frame, but is provided in the world frame
+    Eigen::Vector3d vel;
+    vel << msg->twist.twist.linear.x,
+        msg->twist.twist.linear.y,
+        msg->twist.twist.linear.z;
+    vel = world_to_base.inverse() * vel;
+
+    pose_ground_truth.vel[0] = vel(0);
+    pose_ground_truth.vel[1] = vel(1);
+    pose_ground_truth.vel[2] = vel(2);
+
+    pose_ground_truth.rotation_rate[0] = msg->twist.twist.angular.x;
+    pose_ground_truth.rotation_rate[1] = msg->twist.twist.angular.y;
+    pose_ground_truth.rotation_rate[2] = msg->twist.twist.angular.z;
+
+    lcmPublish_.publish("POSE_GROUND_TRUTH", &pose_ground_truth);
+
+    transf_ground_truth.utime = pose_ground_truth.utime;
+    transf_ground_truth.trans[0] = msg->pose.pose.position.x;
+    transf_ground_truth.trans[1] = msg->pose.pose.position.y;
+    transf_ground_truth.trans[2] = msg->pose.pose.position.z;
+
+    transf_ground_truth.quat[0] = msg->pose.pose.orientation.w;
+    transf_ground_truth.quat[1] = msg->pose.pose.orientation.x;
+    transf_ground_truth.quat[2] = msg->pose.pose.orientation.y;
+    transf_ground_truth.quat[3] = msg->pose.pose.orientation.z;
+
+
+    lcmPublish_.publish("ODOM_GROUND_TRUTH", &transf_ground_truth);
+}
+
 
   verbose_ = false;
 };
 
-App::~App()  {
+void App::pose_vicon_cb(const geometry_msgs::TransformStampedConstPtr& msg) {
+    Eigen::Vector3d pos;
+    Eigen::Vector3d vicon_vel;
+    Eigen::Affine3d a = Eigen::Affine3d::Identity();
+
+
+    a.translation() << msg->transform.translation.x,
+                  msg->transform.translation.y,
+                  msg->transform.translation.z;
+
+    Eigen::Quaterniond a_q(msg->transform.rotation.w,
+                           msg->transform.rotation.x,
+                           msg->transform.rotation.y,
+                           msg->transform.rotation.z);
+    a.rotate(a_q);
+
+    if(first_vicon_transform) {
+        tf::StampedTransform transform;
+        try {
+            listener_.waitForTransform(vicon_frame,
+                                       "base_link",
+                                       ros::Time(0),
+                                       ros::Duration(10.0));
+
+            listener_.lookupTransform(vicon_frame,
+                                      "base_link",
+                                      ros::Time(0),
+                                      transform);
+
+            tf::transformTFToEigen(transform, bTf);
+            ROS_INFO("First Vicon-to-base transform captured:");
+            std::cout << bTf.matrix() << std::endl;
+            first_vicon_transform = false;
+            Eigen::Affine3d c = a * bTf;
+            pos << c.translation().x(), c.translation().y(), c.translation().z();
+
+            vd_->updateMA(msg->header.stamp.toSec(), pos, vicon_vel);
+            return;
+
+        } catch (tf::TransformException ex) {
+            std::cerr << "ERROR: vicon transform not received!" << std::endl;
+            std::cerr << ex.what() << std::endl;
+            return;
+        }
+    } else {
+        Eigen::Affine3d c = a * bTf;
+        bot_core::rigid_transform_t pose_msg;
+        pose_msg.utime = (int64_t) floor(msg->header.stamp.toNSec() / 1000);
+        pose_msg.trans[0] = msg->transform.translation.x;
+        pose_msg.trans[1] = msg->transform.translation.y;
+        pose_msg.trans[2] = msg->transform.translation.z;
+        pose_msg.quat[0] =  msg->transform.rotation.w;
+        pose_msg.quat[1] =  msg->transform.rotation.x;
+        pose_msg.quat[2] =  msg->transform.rotation.y;
+        pose_msg.quat[3] =  msg->transform.rotation.z;
+        lcmPublish_.publish("VICONSYSTEM_TO_LOCAL", &pose_msg);
+
+        bot_core::pose_t pose_msg3;
+
+        /* getting the estimated velocity */
+        pos << c.translation().x(), c.translation().y(), c.translation().z();
+        vd_->updateMA(msg->header.stamp.toSec(), pos, vicon_vel);
+
+        Eigen::Affine3d world_to_base = Eigen::Affine3d::Identity();
+        world_to_base.rotate(c.rotation());
+
+
+        // Velocity is expressed in the world frame.
+        //We rotate so its in base frame
+        vicon_vel = world_to_base.inverse() * vicon_vel;
+
+        pose_msg3.utime = (int64_t) floor(msg->header.stamp.toNSec() / 1000);
+        pose_msg3.pos[0] = c.translation().x();
+        pose_msg3.pos[1] = c.translation().y();
+        pose_msg3.pos[2] = c.translation().z();
+        pose_msg3.vel[0] = vicon_vel(0);
+        pose_msg3.vel[1] = vicon_vel(1);
+        pose_msg3.vel[2] = vicon_vel(2);
+        pose_msg3.accel[0] = 0;
+        pose_msg3.accel[1] = 0;
+        pose_msg3.accel[2] = 0;
+
+        Eigen::Quaterniond c_q = Eigen::Quaterniond(c.rotation());
+        pose_msg3.orientation[0] =  c_q.w();
+        pose_msg3.orientation[1] =  c_q.x();
+        pose_msg3.orientation[2] =  c_q.y();
+        pose_msg3.orientation[3] =  c_q.z();
+
+        pose_msg3.rotation_rate[0] = 0;
+        pose_msg3.rotation_rate[1] = 0;
+        pose_msg3.rotation_rate[2] = 0;
+
+        if(send_pose_body_) {
+            lcmPublish_.publish("POSE_BODY", &pose_msg3);
+        }
+        lcmPublish_.publish("POSE_VICON", &pose_msg3);
+
+        bot_core::rigid_transform_t pose_msg4;
+        pose_msg4.utime = (int64_t) floor(msg->header.stamp.toNSec() / 1000);
+        pose_msg4.trans[0] = c.translation().x();
+        pose_msg4.trans[1] = c.translation().y();
+        pose_msg4.trans[2] = c.translation().z();
+        pose_msg4.quat[0] =  c_q.w();
+        pose_msg4.quat[1] =  c_q.x();
+        pose_msg4.quat[2] =  c_q.y();
+        pose_msg4.quat[3] =  c_q.z();
+        lcmPublish_.publish("VICON_TO_LOCAL", &pose_msg4);
+    }
 }
 
 
