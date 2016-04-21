6f0a3b925b0e095e029b197b8b75eda455cc53e6
Marco Camurri <marco.camurri@iit.it>
2016-04-21 13:03:13 +0200

    fixed the effects of a old bad merge result

--- pronto-lcm-ros-translators/src/pronto_translators/src/ros2lcm_hyq.cpp
+++ pronto-lcm-ros-translators/src/pronto_translators/src/ros2lcm_hyq.cpp
@@ -27,6 +27,7 @@
 
 #include <lcm/lcm-cpp.hpp>
 #include <lcmtypes/bot_core.hpp>
+#include <ViconDerivator.hpp>
 
 using namespace std;
 
@@ -77,7 +78,6 @@ private:
     ViconDerivator* vd_;
     std::string vicon_frame = "vicon/hyq_green/body";
     lcm::LCM lcm_publish_;
-      bot_core::ins_t imu;
 };
 
 App::App(ros::NodeHandle& node,
@@ -126,128 +126,6 @@ App::App(ros::NodeHandle& node,
 
 }
 
-  //joint_states_sub_ = node_.subscribe(string("joint_states"), 100, &App::joint_states_cb,this);
-
-void App::simGroundTruthCallback(const nav_msgs::OdometryConstPtr &msg) {
-    pose_ground_truth.utime = msg->header.stamp.toNSec() / 1000;
-
-    Eigen::Affine3d world_to_base = Eigen::Affine3d::Identity();
-    Eigen::Quaterniond msg_orient(msg->pose.pose.orientation.w,
-                                  msg->pose.pose.orientation.x,
-                                  msg->pose.pose.orientation.y,
-                                  msg->pose.pose.orientation.z);
-    world_to_base.rotate(msg_orient);
-
-    pose_ground_truth.pos[0] = msg->pose.pose.position.x;
-    pose_ground_truth.pos[1] = msg->pose.pose.position.y;
-    pose_ground_truth.pos[2] = msg->pose.pose.position.z;
-
-    pose_ground_truth.orientation[0] = msg->pose.pose.orientation.w;
-    pose_ground_truth.orientation[1] = msg->pose.pose.orientation.x;
-    pose_ground_truth.orientation[2] = msg->pose.pose.orientation.y;
-    pose_ground_truth.orientation[3] = msg->pose.pose.orientation.z;
-
-    // Velocity must be in the base frame, but is provided in the world frame
-    Eigen::Vector3d vel;
-    vel << msg->twist.twist.linear.x,
-        msg->twist.twist.linear.y,
-        msg->twist.twist.linear.z;
-    vel = world_to_base.inverse() * vel;
-
-    pose_ground_truth.vel[0] = vel(0);
-    pose_ground_truth.vel[1] = vel(1);
-    pose_ground_truth.vel[2] = vel(2);
-
-private:
-    Eigen::Affine3d bTf; // Transform between viconplate-to-body
-    bool first_vicon_transform;
-    bool send_ground_truth_; // publish control msgs to LCM
-    bool send_pose_body_;
-    lcm::LCM lcmPublish_ ;
-    ros::NodeHandle node_;
-
-    // Atlas Joints and FT sensor
-    ros::Subscriber  joint_states_sub_;
-    void joint_states_cb(const sensor_msgs::JointStateConstPtr& msg);
-    std::vector<double> ptu_position_, ptu_velocity_, ptu_effort_;
-    std::vector<std::string> ptu_name_;
-
-    // The position and orientation from a vicon system:
-    ros::Subscriber pose_vicon_sub_;
-    ros::Subscriber pose_sim_ground_truth;
-    void pose_vicon_cb(const geometry_msgs::TransformStampedConstPtr& msg);
-    void pose_vicon_alt_cb(const geometry_msgs::TransformStampedConstPtr& msg);
-
-    ros::Subscriber imuSensorSub_;
-    void imuSensorCallback(const sensor_msgs::ImuConstPtr& msg);
-
-    ros::Subscriber laserScanSub_;
-    //void laserScanCallback(const sensor_msgs::LaserScanConstPtr& msg);
-
-    void simGroundTruthCallback(const nav_msgs::OdometryConstPtr& msg);
-
-    void publishLidar(const sensor_msgs::LaserScanConstPtr& msg, string channel );
-
-    int64_t last_joint_state_utime_;
-    bool verbose_;
-    bot_core::ins_t imu;
-    bot_core::pose_t pose_ground_truth;
-    bot_core::rigid_transform_t transf_ground_truth;
-
-    tf::TransformListener listener_;
-    ViconDerivator* vd_;
-    std::string vicon_frame = "vicon/hyq_green/body";
-    lcm::LCM lcm_publish_;
-};
-
-App::App(ros::NodeHandle& node,
-         bool send_ground_truth,
-         bool send_pose_body) :
-
-    send_ground_truth_(send_ground_truth),
-    node_(node),
-    send_pose_body_(send_pose_body),
-    first_vicon_transform(true),
-    bTf(Eigen::Affine3d::Identity()),
-    vd_(new ViconDerivator(5)) {
-
-    ROS_INFO("Initializing Translator");
-    if(!lcmPublish_.good()) {
-        std::cerr << "ERROR: lcm is not good()" << std::endl;
-    }
-
-    for(int i = 0; i < 4; i++) {
-        imu.quat[i] = 0;
-        if(i < 3) {
-            imu.accel[i] = 0;
-            imu.gyro[i] = 0;
-            imu.mag[i] = 0;
-        }
-    }
-    imu.pressure = 0;
-    imu.rel_alt = 0;
-
-    joint_states_sub_ = node_.subscribe(string("joint_states"), 100, &App::joint_states_cb, this);
-    ptu_name_ = {"ptu_pan", "ptu_tilt"};
-    ptu_position_ = {0.0, 0.0};
-    ptu_velocity_ = {0.0, 0.0};
-    ptu_effort_ = {0.0, 0.0};
-
-    pose_vicon_sub_ = node_.subscribe(vicon_frame, 100, &App::pose_vicon_cb, this);
-    pose_vicon_sub_ = node_.subscribe("/hyq2max/ground_truth_odom", 100, &App::simGroundTruthCallback, this);
-    imuSensorSub_ = node_.subscribe("/imu/imu", 100, &App::imuSensorCallback, this);
-
-    //laserScanSub_ = node_.subscribe("scan", 100, &App::laserScanCallback, this);
-
-    verbose_ = false;
-    ROS_INFO("ros2lcm Translator ready");
-}
-
-  //joint_states_sub_ = node_.subscribe(string("joint_states"), 100, &App::joint_states_cb,this);
-  //ptu_name_ = {"ptu_pan", "ptu_tilt"};
-  //ptu_position_ = {0.0,0.0};
-  //ptu_velocity_ = {0.0,0.0};
-  //ptu_effort_ = {0.0,0.0};
 
 void App::simGroundTruthCallback(const nav_msgs::OdometryConstPtr &msg) {
     pose_ground_truth.utime = msg->header.stamp.toNSec() / 1000;
@@ -479,56 +357,6 @@ void App::joint_states_cb(const sensor_msgs::JointStateConstPtr& msg){
 
 int gt_counter = 0;
 
-void App::pose_bdi_cb(const nav_msgs::OdometryConstPtr& msg){
-  if (gt_counter%1000 ==0){
-    ROS_ERROR("BDI  [%d]", gt_counter );
-  }  
-  gt_counter++;
-
-  bot_core::pose_t pose_msg;
-  pose_msg.utime = (int64_t) floor(msg->header.stamp.toNSec()/1000);
-
-  if (verbose_)
-    std::cout <<"                                                            " << pose_msg.utime << " bdi\n";
-
-  pose_msg.pos[0] = msg->pose.pose.position.x;
-  pose_msg.pos[1] = msg->pose.pose.position.y;
-  pose_msg.pos[2] = msg->pose.pose.position.z;
-  // what about orientation in imu msg?
-  pose_msg.orientation[0] =  msg->pose.pose.orientation.w;
-  pose_msg.orientation[1] =  msg->pose.pose.orientation.x;
-  pose_msg.orientation[2] =  msg->pose.pose.orientation.y;
-  pose_msg.orientation[3] =  msg->pose.pose.orientation.z;
-
-  // This transformation is NOT correct for Trooper
-  // April 2014: added conversion to body frame - so that both rates are in body frame
-  Eigen::Matrix3d R = Eigen::Matrix3d( Eigen::Quaterniond( msg->pose.pose.orientation.w, msg->pose.pose.orientation.x,
-                                                           msg->pose.pose.orientation.y, msg->pose.pose.orientation.z ));
-  Eigen::Vector3d lin_body_vel  = R*Eigen::Vector3d ( msg->twist.twist.linear.x, msg->twist.twist.linear.y,
-                                                      msg->twist.twist.linear.z );
-  pose_msg.vel[0] = lin_body_vel[0];
-  pose_msg.vel[1] = lin_body_vel[1];
-  pose_msg.vel[2] = lin_body_vel[2];
-
-
-  // this is the body frame rate
-  pose_msg.rotation_rate[0] = msg->twist.twist.angular.x;
-  pose_msg.rotation_rate[1] = msg->twist.twist.angular.y;
-  pose_msg.rotation_rate[2] = msg->twist.twist.angular.z;
-  // prefer to take all the info from one source
-//  pose_msg.rotation_rate[0] = imu_msg_.angular_velocity.x;
-//  pose_msg.rotation_rate[1] = imu_msg_.angular_velocity.y;
-//  pose_msg.rotation_rate[2] = imu_msg_.angular_velocity.z;
-  
-  // Frame?
-  //pose_msg.accel[0] = imu_msg_.linear_acceleration.x;
-  //pose_msg.accel[1] = imu_msg_.linear_acceleration.y;
-  //pose_msg.accel[2] = imu_msg_.linear_acceleration.z;
-
-  lcm_publish_.publish("POSE_BDI", &pose_msg);   
-  // lcm_publish_.publish("POSE_BODY", &pose_msg);    // for now
-}
-
 
 void App::pose_vicon_alt_cb(const geometry_msgs::TransformStampedConstPtr& msg) {
 
@@ -594,7 +422,6 @@ void App::pose_vicon_alt_cb(const geometry_msgs::TransformStampedConstPtr& msg) 
 
 void App::imuSensorCallback(const sensor_msgs::ImuConstPtr& msg) {
 
-  bot_core::ins_t imu;
   imu.utime = (int64_t) floor(msg->header.stamp.toNSec()/1000);
   imu.device_time = imu.utime;
   imu.gyro[0] = msg->angular_velocity.x;
